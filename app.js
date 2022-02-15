a202be604e816080cfb492aa81e0a6b9c34313ee
Bernard <bernard@hatronika.co.za>
2022-02-15 14:32:47 +0200

    fix unexpected token

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const menuRouter = require('./routes/menuRoutes');
 const userRouter = require('./routes/userRoutes');
 const ingredientRouter = require('./routes/ingredientRoutes');
 
-const Database = require('./services/database')
+const Database = require('./services/database');
 
 //Database Connection
-Database.connect(process.env.DB_URL)
+Database.connect(process.env.DB_URL);
 
 //Middleware
 app.use(express.json());
@@ -28,8 +28,8 @@ app.use(cors({
 
 // Routes
 app.get('/' , (req, res) => {
-    res.sendFile(path.join(__dirname+'/views/home.html'))
-})
+    res.sendFile(path.join(__dirname+'/views/home.html'));
+});
 app.use('/auth', authRoutes);
 app.use('/meals', mealsRouter);
 app.use('/menus', menuRouter);
@@ -39,13 +39,13 @@ app.use('/ingredients', ingredientRouter);
 //Error handling
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if(!err.message) err.message = 'Something went wrong' 
+    if(!err.message) err.message = 'Something went wrong';
     res.status(statusCode).send(err);
-})
+});
 
 // Listener
-port = process.env.PORT || 8080
+port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log('MUNCHIE SERVER')
-    console.log(`Listening on port ${port`)
-})
+    console.log('MUNCHIE SERVER');
+    console.log(`Listening on port ${port}`);
+});
