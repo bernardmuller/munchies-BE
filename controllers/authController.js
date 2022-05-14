dea97baadbe4465faa23763903c7cb81017ad00f
Bernard <b.mullerjnr@gmail.com>
2022-05-14 23:14:41 +0200

    auto login if  JWT still valid

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const AuthService = require("../services/authService");
+const User = require("../models/User");
+const AppError = require("../utils/AppError");
 const authService = new AuthService();
 dotenv.config();
 
 module.exports = class AuthController {
 	contructor() {}
 
-	register = async function(req, res) {
+	register = async function (req, res) {
 		try {
 			const user_data = {
 				email: req.body.email,
@@ -18,34 +20,38 @@ module.exports = class AuthController {
 
 			if (!data.password) res.status(401).send("no password provided");
 
-            const response = await AuthService.register(user_data)
+			const response = await AuthService.register(user_data);
 
 			res.status(201).json(response);
 		} catch (error) {
-            console.log(error)
+			console.log(error);
 			// res.status(400).json({ errors });
-		};
+		}
 	};
 
-	login = async function(req, res) {
+	login = async function (req, res) {
 		try {
-            
-            const user_data = {
+			const user_data = {
 				email: req.body.email,
 				password: req.body.password,
 			};
 
-			if (user_data.email == undefined) res.status(401).send("no email provided");
+			if (user_data.email == undefined)
+				res.status(401).send("no email provided");
 
-			if (user_data.password == undefined) res.status(401).send("no password provided");
+			if (user_data.password == undefined)
+				res.status(401).send("no password provided");
 
-            const response = await authService.login(user_data.email, user_data.password);
+			const response = await authService.login(
+				user_data.email,
+				user_data.password
+			);
 
 			res.status(200).json(response);
 		} catch (error) {
 			console.log(error);
 			// res.status(400).send({ errors });
-		};
+		}
 	};
 
 	logout = async (req, res) => {
@@ -63,22 +69,18 @@ module.exports = class AuthController {
 	};
 
 	auth = async (req, res) => {
-		let currentUser = res.locals.currentUser;
-		if (!currentUser)
-			return res
-				.status(401)
-				.json({ auth: false, message: "Unauthorized" });
-
-		jwt.verify(token, `${process.env.JWT_SECRET}`, (err, decodedtoken) => {
-			if (err)
+		try {
+			if (!res.locals.user)
 				return res
 					.status(401)
-					.json({ auth: false, message: err.message });
-			res.status(200).json({ auth: true, message: "Authorized" });
-		});
-	};
+					.json({ auth: false, message: "Unauthorized" });
 
-    bark = () => {
-        console.log('bark')
-    }
+			const currentUser = await User.findById(res.locals.user);
+			if (!currentUser)
+				res.status(401).send({ auth: false, message: "Unauthorized" });
+			return res.status(200).send({ auth: true });
+		} catch (error) {
+			throw new AppError(error, 500);
+		}
+	};
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,29 +1,29 @@
-const AuthController = require('../controllers/authController');
+const AuthController = require("../controllers/authController");
 const authController = new AuthController();
 
 module.exports.authRoutes = {
-    auth : {
-        method: 'GET',
-        path: '/auth',
-        handler: authController.auth,
-        auth: false
-    },
-    register : {
-        method: 'POST',
-        path: '/auth/register',
-        handler: authController.register,
-        auth: false
-    },
-    login : {
-        method: 'POST',
-        path: '/auth/login',
-        handler: authController.login,
-        auth: false
-    },
-    logout : {
-        method: 'GET',
-        path: '/auth/logout',
-        handler: authController.logout,
-        auth: true
-    },
+	auth: {
+		method: "GET",
+		path: "/auth",
+		handler: authController.auth,
+		auth: true,
+	},
+	register: {
+		method: "POST",
+		path: "/auth/register",
+		handler: authController.register,
+		auth: false,
+	},
+	login: {
+		method: "POST",
+		path: "/auth/login",
+		handler: authController.login,
+		auth: false,
+	},
+	logout: {
+		method: "GET",
+		path: "/auth/logout",
+		handler: authController.logout,
+		auth: true,
+	},
 };
